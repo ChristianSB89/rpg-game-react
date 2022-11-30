b1992cbb072cc2e9fc290ac95b025b9dfa42fa53
Christian SB <christiansb.kodehode@gmail.com>
2022-11-30 14:26:30 +0100

    Setup file structure

---
+++ src/tile-view/slices/characterSlice.js
@@ -0,0 +1,24 @@
+import { characterSlice, createSlice } from "@reduxjs/toolkit";
+
+const characterSlice = createSlice({
+  name: "character",
+  initialState: {
+    x: 6,
+    y: 6,
+    heroClass: "FLAME_SWORDSMAN",
+    heroImg: null,
+  },
+  reducers: {
+    move(state, action) {
+      const [x, y] = action.payload;
+      state.x += y;
+      state.y += y;
+    },
+    bufferImage(state, action) {
+      state.heroImg = action.payload;
+    },
+  },
+});
+
+export const { move, bufferImage } = characterSlice.actions;
+export default characterSlice.reducer;
